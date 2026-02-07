60956b881dad67e8c1ad8f80fb57043dbb33f74e
Noah Achord <nmachord@coastal.edu>
2026-02-07 16:55:55 -0500

    Added a writeln

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace cs330courses
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
+            Console.WriteLine("My name is Noah");
             CourseServices service = new CourseServices();
 
             Console.WriteLine("---------");
